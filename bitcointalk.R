ead1f4c0bc4c16ff67c5ea1cfede93b467560d32
Chih-Cheng Liang <chihchengliang@gmail.com>
2016-09-28 15:50:34 +0800

    bitcointalk.org

---
+++ bitcointalk.R
@@ -0,0 +1,38 @@
+library(lubridate)
+library(httr)
+library(xml2)
+library(dplyr)
+
+extract_data <- function(yyyymm){
+  url <- paste0(
+    "https://bitcointalk.org/index.php?action=stats;expand=",
+    yyyymm, ";xml")
+  
+  doc <- GET(
+    url,
+    config = list(
+      "Referer"="https://bitcointalk.org/index.php?action=stats"
+      )
+    )
+  df <- doc %>%
+    content() %>%
+    xml_nodes("day") %>%
+    xml_attrs() %>%
+    lapply(as.list) %>%
+    bind_rows()
+  return(df)
+}
+# extract_data("200912")
+
+dates <- seq(
+  ymd("2009-12-01"),
+  ymd("2016-09-01"),
+  by = "1 month") %>%
+  format("%Y%m")
+
+df_list <- dates %>% lapply(extract_data)
+bitcointalk <- df_list %>% bind_rows() %>%
+  mutate_each(funs(as.numeric), 2:6) %>%
+  mutate(date = as.Date(date))
+
+bitcointalk %>% write.csv("bitcointalk.csv", row.names = F)
